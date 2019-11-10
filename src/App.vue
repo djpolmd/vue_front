92a04178d0a4941900b754ccf3b3504e26006049
djpolmd <djpolmd@gmail.com>
2019-11-10 17:40:43 +0200

    new repo

---
+++ src/App.vue
@@ -0,0 +1,66 @@
+<template>
+    <v-app>
+        <v-toolbar>
+            <v-toolbar-title><v-btn flat :to="{name:'Home'}">{{title}}</v-btn></v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-toolbar-items class="hidden-sm-and-down">
+                <v-btn flat exact  v-for="(menu,index) in menus" :key='index' :to={name:menu.route}>
+                    {{menu.name}}
+                </v-btn>
+
+
+            </v-toolbar-items>
+        </v-toolbar>
+
+        <router-view></router-view>
+
+        <loader></loader>
+    </v-app>
+</template>
+
+<script>
+    import Loader from './components/partials/_loader'
+    import {mapGetters} from 'vuex'
+    export default {
+        components:{Loader},
+        data () {
+            return {
+                title:"PhoneBook",
+                loader:true
+
+            }
+        },
+        name: 'App',
+        computed:mapGetters(['isAuthenticated','menus']),
+
+        created(){
+
+            axios.interceptors.request.use( (config)=> {
+                // Do something before request is sent
+                this.$store.commit('LOADER',true);
+                return config;
+            },  (error)=> {
+                // Do something with request error
+                this.$store.commit('LOADER',false);
+
+                return Promise.reject(error);
+            });
+
+            axios.interceptors.response.use((response)=>{
+
+                console.log(response);
+                this.$store.commit('LOADER',false);
+                return response;
+
+            }, (err)=> {
+                return new Promise( (resolve, reject)=> {
+                    this.$store.dispatch('logout').then(()=>{
+                        this.$router.push('/login')
+                    })
+                    throw err;
+                });
+            });
+
+        }
+    }
+</script>
---
+++ src/main.js
@@ -0,0 +1,30 @@
+import Vue from 'vue'
+import App from './App'
+import router from './router/index'
+import Vuetify from 'vuetify'
+import axios from 'axios'
+import 'vuetify/dist/vuetify.min.css'
+import store from './store/index'
+
+window.Bus=new Vue;
+window.axios=axios
+window.token= localStorage.getItem('token');
+window.user= localStorage.getItem('user');
+
+axios.defaults.baseURL = 'http://127.0.0.1:8000/api';
+
+axios.defaults.headers.common['Authorization'] = "Bearer " + window.token;
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+
+Vue.use(Vuetify)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+    el: '#app',
+    router,
+    store,
+    components: { App },
+    template: '<App/>'
+})
